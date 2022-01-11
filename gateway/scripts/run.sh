7d25baf42d08b7ec31d57b03dd9e7a3a1fe1b727
Aurélien Perrier <a.perrier89@gmail.com>
2022-01-12 00:06:41 +0100

    Delete tws build

--- gateway/scripts/run.sh
+++ gateway/scripts/run.sh
@@ -9,13 +9,18 @@ Xvfb :0 -ac -screen 0 1920x1080x24 &
 export DISPLAY=:0
 
 # Start VNC server, listening at 5900 by default
-x11vnc -ncache 10 -ncache_cr -display :0 -forever -shared -bg -noipv6 &
-
-# Start nginx reverse proxy
-nginx -c /usr/share/nginx/nginx.conf
+if [ -n "$VNC_SERVER_PASSWORD" ]; then
+    echo "Starting VNC server"
+    x11vnc -ncache 10 -ncache_cr -display :0 -forever -shared -bg -noipv6 -passwd $VNC_SERVER_PASSWORD &
+else
+    x11vnc -ncache 10 -ncache_cr -display :0 -forever -shared -bg -noipv6 &
+fi
 
 # Start noVNC server
 /usr/share/novnc/utils/launch.sh --vnc localhost:5900 &
+
+# Export ports of IB Gateway
+./fork_ports_delayed.sh &
 
 # Start TWS and automatically restart if it dies
 while true; do
