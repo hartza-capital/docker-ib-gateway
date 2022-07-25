e29efbf2a1518d30cb8bd7dcfc0e6b6a30dcbc6a
Aurélien Perrier <a.perrier89@gmail.com>
2022-07-25 09:31:41 +0200

    upgrade: IBC version 3.14.0

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
 #/bin/bash
 PUSH=$1
-IBC_VERSION=3.12.0
+IBC_VERSION=3.14.0
 
 echo "Extract versions"
 BUILD=$(curl -s https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
 MAJOR=$(echo $BUILD | grep -o '^[0-9]*')
 MINOR=$(echo $BUILD | grep -o '^[0-9]*.[0-9]*')
+IMAGE=quay.io/arktos-venture/ibkr-gateway
 
 echo "Build images $BUILD"
-docker build ./gateway -t quay.io/arktos-venture/ibkr-gateway:$BUILD --build-arg IBC_VERSION=$IBC_VERSION --platform linux/amd64
-docker tag quay.io/arktos-venture/ibkr-gateway:$BUILD quay.io/arktos-venture/ibkr-gateway:latest
-docker tag quay.io/arktos-venture/ibkr-gateway:$BUILD quay.io/arktos-venture/ibkr-gateway:$MAJOR
-docker tag quay.io/arktos-venture/ibkr-gateway:$BUILD quay.io/arktos-venture/ibkr-gateway:$MINOR
+docker build ./gateway --platform linux/amd64 --build-arg IBC_VERSION=$IBC_VERSION -t $IMAGE:$BUILD
+docker tag $IMAGE:$BUILD $IMAGE:latest
+docker tag $IMAGE:$BUILD $IMAGE:$MAJOR
+docker tag $IMAGE:$BUILD $IMAGE:$MINOR
 if [ "$PUSH" = "--push" ]; then
     echo "Push images" 
-    docker push quay.io/arktos-venture/ibkr-gateway:$BUILD
-    docker push quay.io/arktos-venture/ibkr-gateway:$MAJOR
-    docker push quay.io/arktos-venture/ibkr-gateway:$MINOR
-    docker push quay.io/arktos-venture/ibkr-gateway:latest
+    docker push $IMAGE:$BUILD
+    docker push $IMAGE:$MAJOR
+    docker push $IMAGE:$MINOR
+    docker push $IMAGE:latest
 fi
