b0edc8f8dc07291fcdd11a6e76b7f69b29452720
Aurélien Perrier <aperrier@arktos.consulting>
2022-12-12 00:49:06 +0100

    fix(gateway): build 2 channels

--- build.sh
+++ build.sh
@@ -1,22 +1,40 @@
 #/bin/bash
-PUSH=$1
-IBC_VERSION=3.14.0
 
-echo "Extract versions"
-BUILD=$(curl -s https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
-MAJOR=$(echo $BUILD | grep -o '^[0-9]*')
-MINOR=$(echo $BUILD | grep -o '^[0-9]*.[0-9]*')
+# Inputs values
+CHANNEL=$1
+PUSH=$2
+
+# Default values
+URL_DOWNLOAD=https://download2.interactivebrokers.com/installers/ibgateway
+IBC_VERSION=3.15.2
 IMAGE=quay.io/arktos-fund/ibkr-gateway
 
-echo "Build images $BUILD"
-docker build ./gateway --platform linux/amd64 --build-arg IBC_VERSION=$IBC_VERSION -t $IMAGE:$BUILD
-docker tag $IMAGE:$BUILD $IMAGE:latest
+echo "Try to extract versions"
+if [ "$CHANNEL" = "latest" ]; then
+    BUILD=$(curl -s $URL_DOWNLOAD/latest-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
+elif [ "$CHANNEL" = "stable" ]; then
+    BUILD=$(curl -s $URL_DOWNLOAD/stable-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
+    IBC_VERSION=3.12.0
+else
+    echo "channel ${CHANNEL} isn't available"
+    exit 1
+fi
+
+# Extract versions
+MAJOR=$(echo $BUILD | grep -o '^[0-9]*')
+MINOR=$(echo $BUILD | grep -o '^[0-9]*.[0-9]*')
+
+# Build & tags
+echo "Build image $BUILD"
+docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_VERSION=$IBC_VERSION -t $IMAGE:$BUILD
+docker tag $IMAGE:$BUILD $IMAGE:$CHANNEL
 docker tag $IMAGE:$BUILD $IMAGE:$MAJOR
 docker tag $IMAGE:$BUILD $IMAGE:$MINOR
+
+# Push images
 if [ "$PUSH" = "--push" ]; then
-    echo "Push images" 
+    echo "Push images Gateway" 
     docker push $IMAGE:$BUILD
-    docker push $IMAGE:$MAJOR
     docker push $IMAGE:$MINOR
-    docker push $IMAGE:latest
+    docker push $IMAGE:$CHANNEL
 fi
