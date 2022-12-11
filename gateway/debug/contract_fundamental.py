3d55a8084c6558d026ac698f656b33157d196991
Aurélien Perrier <aperrier@arktos.consulting>
2022-12-12 00:49:40 +0100

    feat(gateway): debug scripts

---
+++ gateway/debug/contract_fundamental.py
@@ -0,0 +1,14 @@
+import ib_insync as ibis
+
+ibclient = ibis.IB()
+client = ibclient.connect(
+                host="127.0.0.1", 
+                port=4001,
+                clientId=200,
+                readonly=True
+            )
+
+stock = ibis.Stock("AAPL", "SMART", currency="USD")
+
+reqData = client.reqFundamentalData(stock, "ReportSnapshot")
+print(reqData)
