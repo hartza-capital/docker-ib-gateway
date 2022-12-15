3431aa2bd8f0f8ace99e0769ca62c7e9a68f16a4
Aurélien Perrier <aperrier@arktos.fund>
2022-12-15 17:02:44 +0100

    fix(gateway): default values arguments & quay expiration (#14)

--- build.sh
+++ build.sh
@@ -5,17 +5,16 @@ CHANNEL=$1
 PUSH=$2
 
 # Default values
-URL_DOWNLOAD=https://download2.interactivebrokers.com/installers/ibgateway
-IBC_VERSION=3.15.2
+URL_DOWNLOAD=https://download2.interactivebrokers.com/installers/ibgateway/$CHANNEL-standalone/version.json
 IMAGE=quay.io/arktos-fund/ibkr-gateway
 
 echo "Try to extract versions"
 if [ "$CHANNEL" = "latest" ]; then
-    BUILD=$(curl -s $URL_DOWNLOAD/latest-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
+    BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
+    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg QUAY_EXPIRE=12w -t $IMAGE:$BUILD
 elif [ "$CHANNEL" = "stable" ]; then
-    BUILD=$(curl -s $URL_DOWNLOAD/stable-standalone/version.json | grep -o '{.*}' | jq -r .buildVersion)
-    # fix value if the diff of version is too large
-    IBC_VERSION=3.15.2
+    BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
+    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_VERSION=3.15.2 -t $IMAGE:$BUILD
 else
     echo "channel ${CHANNEL} isn't available"
     exit 1
@@ -26,10 +25,9 @@ MAJOR=$(echo $BUILD | grep -o '^[0-9]*')
 MINOR=$(echo $BUILD | grep -o '^[0-9]*.[0-9]*')
 
 # Build & tags
-echo "Build image $BUILD"
-docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_VERSION=$IBC_VERSION -t $IMAGE:$BUILD
+echo "Tags image $BUILD"
 docker tag $IMAGE:$BUILD $IMAGE:$CHANNEL
-docker tag $IMAGE:$BUILD $IMAGE:$MAJOR
+docker tag $IMAGE:$BUILD $IMAGE:$MAJOR-$CHANNEL
 docker tag $IMAGE:$BUILD $IMAGE:$MINOR
 
 # Push images
@@ -37,5 +35,8 @@ if [ "$PUSH" = "--push" ]; then
     echo "Push images Gateway" 
     docker push $IMAGE:$BUILD
     docker push $IMAGE:$MINOR
+    if [ "$CHANNEL" = "stable" ]; then
+        docker push $IMAGE:$MAJOR
+    fi
     docker push $IMAGE:$CHANNEL
 fi
