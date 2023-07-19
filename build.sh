99025a406ffcb3f394323cd72cc38f973a35c0ad
Aurélien Perrier <aperrier@arktos.fund>
2023-07-19 21:31:21 +0200

    chore(build): upgrade ibAlpha `3.18.0` for latest versions (#19)

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Inputs values
 CHANNEL=$1
 PUSH=$2
+IBC_VERSION_LATEST=3.18.0
 
 # Default values
 URL_DOWNLOAD=https://download2.interactivebrokers.com/installers/ibgateway/$CHANNEL-standalone/version.json
@@ -11,13 +12,13 @@ IMAGE=quay.io/arktos-fund/ibkr-gateway
 echo "Try to extract versions"
 if [ "$CHANNEL" = "latest" ]; then
     BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
-    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg QUAY_EXPIRE=12w -t $IMAGE:$BUILD
+    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=12w -t $IMAGE:$BUILD
 elif [ "$CHANNEL" = "stable" ]; then
     BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
     docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL -t $IMAGE:$BUILD
 elif [ "$CHANNEL" = "nightly" ]; then
     BUILD=$(git rev-parse --short HEAD)
-    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=stable --build-arg QUAY_EXPIRE=1w -t $IMAGE:$BUILD
+    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=stable --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=1w -t $IMAGE:$BUILD
 else
     echo "channel ${CHANNEL} isn't available"
     exit 1
