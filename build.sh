65458c516394bd5c7f50eaa639cff09e8160d325
Aurélien Perrier <aperrier@arktos.consulting>
2025-04-03 21:15:13 +0200

    fix(build): switch to Python 3.13

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ CHANNEL=$1
 PUSH=$2
 
 # IBC versions stable
-IBC_RELEASE_STABLE="3.21.1"
+IBC_RELEASE_STABLE="3.21.2"
 IBC_VERSION_STABLE=$(echo "${IBC_RELEASE_STABLE}" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
 # IBC versions latest
-IBC_RELEASE_LATEST="3.21.1"
+IBC_RELEASE_LATEST="3.21.2"
 IBC_VERSION_LATEST=$(echo "${IBC_RELEASE_LATEST}" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
 
 # Default values
@@ -19,13 +19,13 @@ IMAGE=quay.io/hartza-capital/ib-gateway
 echo "Try to extract versions"
 if [ "$CHANNEL" = "latest" ]; then
     BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
-    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_RELEASE=$IBC_RELEASE_LATEST --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=12w -t $IMAGE:$BUILD
+    docker build ./gateway --platform linux/amd64 --build-arg BUILD=$BUILD --build-arg CHANNEL=$CHANNEL --build-arg IBC_RELEASE=$IBC_RELEASE_LATEST --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=12w -t $IMAGE:$BUILD
 elif [ "$CHANNEL" = "stable" ]; then
     BUILD=$(curl -s $URL_DOWNLOAD | grep -o '{.*}' | jq -r .buildVersion)
-    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=$CHANNEL --build-arg IBC_RELEASE=$IBC_RELEASE_STABLE --build-arg IBC_VERSION=$IBC_VERSION_STABLE -t $IMAGE:$BUILD
+    docker build ./gateway --platform linux/amd64 --build-arg BUILD=$BUILD --build-arg CHANNEL=$CHANNEL --build-arg IBC_RELEASE=$IBC_RELEASE_STABLE --build-arg IBC_VERSION=$IBC_VERSION_STABLE -t $IMAGE:$BUILD
 elif [ "$CHANNEL" = "nightly" ]; then
     BUILD=$(git rev-parse --short HEAD)
-    docker build ./gateway --platform linux/amd64 --build-arg CHANNEL=stable --build-arg IBC_RELEASE=$IBC_RELEASE_LATEST --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=1w -t $IMAGE:$BUILD
+    docker build ./gateway --platform linux/amd64 --build-arg BUILD=$BUILD --build-arg CHANNEL=stable --build-arg IBC_RELEASE=$IBC_RELEASE_LATEST --build-arg IBC_VERSION=$IBC_VERSION_LATEST --build-arg QUAY_EXPIRE=1w -t $IMAGE:$BUILD
 else
     echo "channel ${CHANNEL} isn't available"
     exit 1
